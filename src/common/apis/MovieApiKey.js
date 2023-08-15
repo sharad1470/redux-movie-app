0156dd6c320e4ecb442123da4a7fa4dbee9a0e6c
sharad1470 <sharadsingh3609@gmail.com>
2023-08-15 16:18:45 +0530

    commit

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const ApiKey= '631b3629';
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
+import movieApi from '../../common/apis/MovieApi';
+import {ApiKey} from '../../common/apis/MovieApiKey'
+
 const Home = () => {
+
+    useEffect(()=>{
+
+        const fetchMovies= async()=>{
+            const movieText="Harry";
+            const response=await movieApi.get(`?apiKey=${ApiKey}&s=${movieText}&type=movie`)
+            .catch((err)=>{
+                console.log('Err', err);
+            });
+
+            console.log(response);
+        }
+
+
+        fetchMovies();
+
+    },[]);
     return (
         <div className='banner-img'>
+
 
             <MovieListing/>
         </div>
