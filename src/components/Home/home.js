c90e529de83e24e5513e62c45985c5adb39b6a7f
sharad1470 <sharadsingh3609@gmail.com>
2023-08-15 18:43:49 +0530

    commit

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
 import movieApi from '../../common/apis/MovieApi';
 import {ApiKey} from '../../common/apis/MovieApiKey'
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
+    const dispatch=useDispatch();
 
     useEffect(()=>{
 
@@ -14,13 +17,14 @@ const Home = () => {
                 console.log('Err', err);
             });
 
-            console.log(response);
+           dispatch(addMovies(response.data));
+            //console.log(response);
         }
 
 
         fetchMovies();
 
-    },[]);
+    },[dispatch]);
     return (
         <div className='banner-img'>
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {store} from './features/movies/store';
+import {store} from './features/store';
 import {Provider} from 'react-redux';
 
 
